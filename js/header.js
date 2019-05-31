0135da4461b7fce75b7464277463b73dbba881b1
Shafeeq <idc.mohdshafeeq@gmail.com>
2019-05-31 11:11:48 +0530

    new Changes

---
+++ js/header.js
@@ -0,0 +1,12 @@
+if (document.documentElement.clientWidth > 980) {
+$(window).scroll(function () {
+    var sc = $(window).scrollTop()
+    if (sc > 80) {
+        $(".header").addClass("white-bg");
+    } else {
+        $(".header").removeClass("white-bg");
+    }
+});
+}
+
+
